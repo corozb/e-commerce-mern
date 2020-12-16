f0f88fe9e60f27e23a7b094251e22e8d308046c5
Cristian Orozco <cristianorozco@gmail.com>
2020-12-16 13:04:33 -0500

    Create sample products in MongoDB

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import expressAsyncHanlder from 'express-async-handler'
+import expressAsyncHandlder from 'express-async-handler'
 
 import data from '../data.js'
 import User from '../models/userModel.js'
@@ -8,7 +8,7 @@ const userRouter = express.Router()
 
 userRouter.get(
   '/seed',
-  expressAsyncHanlder(async (req, res) => {
+  expressAsyncHandlder(async (req, res) => {
     // await User.remove({})
     const createdUsers = await User.insertMany(data.users)
     res.send({ createdUsers })
